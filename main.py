c38bb3a915113d6aff9c23e60a85271c375b71cd
Bailey McWaters <125740098+baileymcwaters@users.noreply.github.com>
2025-11-20 10:49:28 +0000

    notification implementation

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import curses
 from curses import wrapper
 from plyer import notification
 
+def send_notification(title: str, message: str) -> None:
+    try:
+        notification.notify(
+            title=title,
+            message=message,
+            app_name='Pomodoro',
+            timeout=5
+        )
+    except Exception:
+        # If notifications fail (e.g., environment not supported), ignore
+        pass
+
 def get_time():
     return time.monotonic()
 
@@ -25,11 +37,9 @@ def main(stdscr) -> None:
     stdscr.timeout(200)
     clock = ss_dur
     last_tick = get_time()
-    notification.notify(
-        title='25 minutes pomodoro remaining',
-        message='Pomodoro will last for 25 minutes',
-        app_name='Pomodoro',
-        timeout=5
+    send_notification(
+        title='Pomodoro started',
+        message=f'Focused session for {ss_dur // 60} minutes'
     )
     while True:
         if is_study:
@@ -81,18 +91,34 @@ def main(stdscr) -> None:
                             is_sb = False
                             sb_completed = sb_completed + 1
                             clock = ss_dur
+                            send_notification(
+                                title='Pomodoro started',
+                                message=f'Focused session for {ss_dur // 60} minutes'
+                            )
                         elif not is_study and not is_sb:
                             is_study = True
                             clock = ss_dur
                             sb_completed = 0
+                            send_notification(
+                                title='Pomodoro started',
+                                message=f'Focused session for {ss_dur // 60} minutes'
+                            )
                         elif is_study and sb_completed == 3:
                             is_study = False
                             is_sb = False
                             clock = lb_dur
+                            send_notification(
+                                title='Long break started',
+                                message=f'Long break for {lb_dur // 60} minutes'
+                            )
                         else:
                             is_study = False
                             is_sb = True
                             clock = sb_dur
+                            send_notification(
+                                title='Short break started',
+                                message=f'Short break for {sb_dur // 60} minutes'
+                            )
 
 if __name__ == '__main__':
     wrapper(main)
